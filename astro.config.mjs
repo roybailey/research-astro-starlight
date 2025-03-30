201e050340b96d155e2498dd0870f4a01691a1a0
Roy Bailey <roybaileybiz@gmail.com>
2025-03-30 16:01:03 +0100

    mermaid diagrams

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,60 +1,70 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mdx from '@astrojs/mdx';
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import expressiveCode from 'astro-expressive-code';
+import remarkMermaid from "remark-mermaidjs";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
+    vite: {
         plugins: [tailwindcss()],
     },
+    markdown: {
+        // Applied to .md and .mdx files
+        remarkPlugins: [remarkMermaid],
+    },
     integrations: [
+        expressiveCode(),
+        mdx(),
         react(),
         starlight({
-            title: 'Research Astro Starlight',
-            customCss: ['./src/assets/css/global.css'],
-            social: {
-                github: 'https://github.com/withastro/starlight',
+        title: 'Research Astro Starlight',
+        routeMiddleware: './src/routeData.ts',
+        customCss: ['./src/assets/css/global.css'],
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        components: {
+            // Override the default `SocialIcons` component.
+            // Header: './src/components/layout/header.astro',
+            // PageFrame: './src/components/PageFrame.astro',
+            SocialIcons: './src/components/layout/social.astro',
+            SiteTitle: './src/components/layout/logo.astro',
+            ThemeSelect: './src/components/layout/ThemeSelect.astro',
+        },
+        sidebar: [
+            {
+                label: 'Home',
+                link: '/'
             },
-            components: {
-                // Override the default `SocialIcons` component.
-                // Header: './src/components/layout/header.astro',
-                // PageFrame: './src/components/PageFrame.astro',
-                SocialIcons: './src/components/layout/social.astro',
-                SiteTitle: './src/components/layout/logo.astro',
-                ThemeSelect: './src/components/layout/ThemeSelect.astro',
+            {
+                label: 'Custom Starlight Page',
+                link: '/samples'
             },
-            sidebar: [
-                {
-                    label: 'Home',
-                    link: '/'
-                },
-                {
-                    label: 'Custom Starlight Page',
-                    link: '/samples'
-                },
-                {
-                    label: 'Astro+React (not Starlight)',
-                    link: '/landing',
-                    badge: { text: 'Not Working', variant: 'caution' },
-                },
-                {
-                    label: 'Guides',
-                    items: [
-                        // Each item here is one entry in the navigation menu.
-                        {label: 'Example Guide', slug: 'guides/example'},
-                    ],
-                },
-                {
-                    label: 'Reference',
-                    autogenerate: {directory: 'reference'},
-                },
-            ],
-        })],
-	experimental: {
-		svg: {
-			mode: "sprite",
-		},
-	},
+            {
+                label: 'Astro+React (not Starlight)',
+                link: '/landing',
+                badge: { text: 'Not Working', variant: 'caution' },
+            },
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    {label: 'Example Guide', slug: 'guides/example'},
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: {directory: 'reference'},
+            },
+        ],
+    })],
+    experimental: {
+        svg: {
+            mode: "sprite",
+        },
+    },
 });
---
+++ src/routeData.ts
@@ -0,0 +1,9 @@
+import { defineRouteMiddleware } from '@astrojs/starlight/route-data';
+
+export const onRequest = defineRouteMiddleware((context) => {
+    // Get the content collection entry for this page.
+    const { entry } = context.locals.starlightRoute;
+    // Update the title to add an exclamation mark.
+    entry.data.title = entry.data.title + '!';
+    console.log(JSON.stringify(entry.data))
+});
