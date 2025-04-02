948412a2ce050387f3019799eb25e6e97c21ea44
Roy Bailey <roybaileybiz@gmail.com>
2025-04-02 10:33:22 +0100

    mermaid

--- src/routeData.ts
+++ src/routeData.ts
@@ -4,6 +4,6 @@ export const onRequest = defineRouteMiddleware((context) => {
     // Get the content collection entry for this page.
     const { entry } = context.locals.starlightRoute;
     // Update the title to add an exclamation mark.
-    entry.data.title = entry.data.title + '!';
+    console.log(JSON.stringify(context.url,null,2))
     console.log(JSON.stringify(entry.data))
 });
